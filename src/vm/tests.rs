2b05da5d44d712c19418b3e70b3c9f3df32e9ac4
ciathefed <ciathefed@protonmail.com>
2025-06-21 00:17:40 -0400

    test(vm): add initial VM unit tests

---
+++ src/vm/tests.rs
@@ -0,0 +1,70 @@
+use crate::{compiler::Compiler, lexer::Lexer, parser::Parser};
+
+use super::*;
+
+use anyhow::Result;
+use pretty_assertions::{assert_eq, assert_ne};
+
+const TEST_MEM_SIZE: usize = 1024;
+
+fn run(input: &str) -> Result<VM> {
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
+    let mut compiler = Compiler::new(parser.parse()?);
+    let mut vm = VM::new(Vec::from(compiler.compile()?), TEST_MEM_SIZE);
+    vm.mem
+        .write(0x00, Immediate::QWord(1337), DataSize::QWord)?;
+    vm.run()?;
+    Ok(vm)
+}
+
+#[test]
+fn hlt() -> Result<()> {
+    let input = r#"hlt"#;
+    let vm = run(input)?;
+
+    assert_eq!(vm.halted, true);
+    Ok(())
+}
+
+#[test]
+fn nop() -> Result<()> {
+    let input = r#"
+        nop
+        hlt
+    "#;
+    let vm = run(input)?;
+
+    assert_eq!(vm.halted, true);
+    assert_eq!(vm.regs.ip, 2);
+    Ok(())
+}
+
+#[test]
+fn mov() -> Result<()> {
+    let input = r#"
+        mov q0, 1337
+        mov d0, q0
+        hlt
+    "#;
+    let vm = run(input)?;
+
+    assert_eq!(vm.halted, true);
+    assert_eq!(vm.regs.ip, 14);
+    assert_eq!(vm.regs.get(Register::Q0), Immediate::QWord(1337));
+    assert_eq!(vm.regs.get(Register::D0), Immediate::DWord(1337));
+    Ok(())
+}
+
+#[test]
+fn ldr() -> Result<()> {
+    let input = r#"
+        ldr q0, [d0, 0]
+        hlt
+    "#;
+    let vm = run(input)?;
+    assert_eq!(vm.halted, true);
+    assert_eq!(vm.regs.ip, 13);
+    assert_eq!(vm.regs.get(Register::Q0), Immediate::QWord(1337));
+    Ok(())
+}
